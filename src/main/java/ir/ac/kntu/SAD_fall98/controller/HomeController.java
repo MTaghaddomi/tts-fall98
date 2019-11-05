19d7f0284a8c5cff66f6d8a9045a2bf8442df9e2
Morteza Taghaddomi Masoumi <mortezataqaddomi99@gmail.com>
2019-11-05 16:54:46 +0330

    add /home mapping

--- src/main/java/ir/ac/kntu/SAD_fall98/controller/HomeController.java
+++ src/main/java/ir/ac/kntu/SAD_fall98/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping({"/", "home"})
     public String home() {
         return "homepage";
     }
