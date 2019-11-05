92366fd9da2c2d13babd654f7b13d2a26581bbd5
Morteza Taghaddomi Masoumi <mortezataqaddomi99@gmail.com>
2019-11-05 16:53:59 +0330

    test entity added

--- src/main/java/ir/ac/kntu/SAD_fall98/SadFall98Application.java
+++ src/main/java/ir/ac/kntu/SAD_fall98/SadFall98Application.java
@@ -1,17 +1,36 @@
 package ir.ac.kntu.SAD_fall98;
 
+import ir.ac.kntu.SAD_fall98.model.User;
+import ir.ac.kntu.SAD_fall98.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableTransactionManagement
 public class SadFall98Application {
+    @Autowired
+    UserRepository userRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(SadFall98Application.class, args);
-	}
+    @Component
+    class DataSetup implements ApplicationRunner {
+
+        @Override
+        public void run(ApplicationArguments args) throws Exception {
+            User user1 = User.builder().username("admin").password("adminadmin").email("admin@gmail.com").build();
+            userRepository.save(user1);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SadFall98Application.class, args);
+    }
 
 }
