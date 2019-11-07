c9d99ca38ec5fdab515173e0f7375481e315eea2
SabaFathi <39127848+SabaFathi@users.noreply.github.com>
2019-11-08 03:29:22 +0330

    add new DTO related to view profile

---
+++ src/main/java/ir/ac/kntu/domain/UserProfileDTO.java
@@ -0,0 +1,22 @@
+package ir.ac.kntu.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserProfileDTO {
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Timestamp birthday;
+    private String phoneNumber;
+}
