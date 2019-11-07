2cf0dabfda15d7d58e3fe3f1b51dce9eb75bc523
SabaFathi <39127848+SabaFathi@users.noreply.github.com>
2019-11-08 03:24:23 +0330

    decrease restrictions

--- src/main/java/ir/ac/kntu/model/User.java
+++ src/main/java/ir/ac/kntu/model/User.java
@@ -13,6 +13,7 @@ import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -33,30 +34,30 @@ public class User {
     private String lastName;
 
     @NotBlank
-    @Length(min = 5, max = 32)
+//    @Length(min = 5, max = 32)
+    @Length(max = 20)
     private String username;
 
-    @NotBlank
 //    @ValidPassword
-    @Length(min = 6, max = 30, message = "password length must be between 0 and 30")
+//    @Length(min = 6, max = 30, message = "password length must be between 0 and 30")
     @NotBlank
     private String password;
 
-    @NotBlank
-    @Email
+//    @NotBlank
+//    @Email
     private String email;
 
-    @CreationTimestamp
-    private LocalDateTime createdAt;
+//    @CreationTimestamp
+    private Timestamp createdAt;
 
-    private LocalDate birthday;
+    private Timestamp birthday;
 
-    @Column(nullable = false, columnDefinition = "TINYINT", length = 1)
-    @ColumnDefault(value = "1")
-    private boolean subscribed;
+//    @Column(nullable = false, columnDefinition = "TINYINT", length = 1)
+//    @ColumnDefault(value = "1")
+//    private boolean subscribed;
 
-    @Length(min = 11, max = 11, message = "phone number should has 11 digit")
-    @Pattern(regexp = "09\\d{9}")
+//    @Length(min = 11, max = 11, message = "phone number should has 11 digit")
+//    @Pattern(regexp = "09\\d{9}")
     private String phoneNumber;
 
 }
