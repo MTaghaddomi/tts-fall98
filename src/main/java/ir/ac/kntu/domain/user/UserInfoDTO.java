2df8f8e8bb19ea845f9fc7ce771832b17f9fc8f2
SabaFathi <39127848+SabaFathi@users.noreply.github.com>
2019-12-13 02:14:45 +0330

    add userInfoDTO

---
+++ src/main/java/ir/ac/kntu/domain/user/UserInfoDTO.java
@@ -0,0 +1,16 @@
+package ir.ac.kntu.domain.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserInfoDTO {
+    private String firstName;
+    private String lastName;
+    private String email;
+}
