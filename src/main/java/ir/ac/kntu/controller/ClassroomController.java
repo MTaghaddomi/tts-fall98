394df02771a7983726967f6705127c789c6fe05a
SabaFathi <39127848+SabaFathi@users.noreply.github.com>
2019-12-13 02:30:53 +0330

    add apis to get detail info and exercises of a classroom

--- src/main/java/ir/ac/kntu/controller/ClassroomController.java
+++ src/main/java/ir/ac/kntu/controller/ClassroomController.java
@@ -1,16 +1,18 @@
 package ir.ac.kntu.controller;
 
-import ir.ac.kntu.domain.classroom.EditableClassroomRequestDto;
-import ir.ac.kntu.domain.classroom.RegisterClassroomRequestDto;
-import ir.ac.kntu.domain.classroom.RegisterClassroomResponseDto;
-import ir.ac.kntu.domain.classroom.SearchClassroomDto;
+import ir.ac.kntu.domain.classroom.*;
+import ir.ac.kntu.domain.submission.ExerciseSubmissionGeneralInfoDTO;
+import ir.ac.kntu.domain.user.UserInfoDTO;
 import ir.ac.kntu.mapper.ClassroomMapper;
 import ir.ac.kntu.model.Classroom;
+import ir.ac.kntu.model.ExerciseSubmission;
+import ir.ac.kntu.model.User;
 import ir.ac.kntu.service.ClassroomService;
 import org.mapstruct.factory.Mappers;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -63,5 +65,58 @@ public class ClassroomController {
     @ResponseStatus(value = HttpStatus.OK)
     public void deleteClassroom(@PathVariable String classroomName) {
         classroomService.deleteClassroom(classroomName);
+    }
+
+    @GetMapping("/{classroomName}")
+    public ClassroomDetailInfoDTO getClassroomDetailInfo(
+            @PathVariable String classroomName){
+        Classroom classroom = classroomService
+                .getClassroomDetailForRequester(classroomName);
+
+        //TODO: use mapper instead
+        ClassroomDetailInfoDTO classroomInfoDTO = new ClassroomDetailInfoDTO();
+        classroomInfoDTO.setName(classroom.getName());
+        classroomInfoDTO.setDescription(classroom.getDescription());
+        classroomInfoDTO.setLesson(new LessonDto(classroom.getLesson().getName(),
+                classroom.getLesson().getDescription()));
+        User teacher = classroom.getTeacher();
+        classroomInfoDTO.setTeacherInfo(new UserInfoDTO(teacher.getFirstName(),
+                teacher.getLastName(), teacher.getEmail()));
+        List<User> students = classroom.getStudents();
+        List<UserInfoDTO> studentsInfo;
+        if(students == null || students.isEmpty()){
+            studentsInfo = null;
+        }else{
+            studentsInfo = new ArrayList<>();
+            for(User s : students){
+                studentsInfo.add(new UserInfoDTO
+                        (s.getFirstName(), s.getLastName(), s.getEmail()));
+            }
+        }
+        classroomInfoDTO.setStudentsInfo(studentsInfo);
+        //
+
+        return classroomInfoDTO;
+    }
+
+    @GetMapping("/{classroomName}/exercises")
+    public List<ExerciseSubmission> getClassroomExercises(
+            @PathVariable String classroomName){
+        List<ExerciseSubmission> exercises = classroomService
+                .getExercises(classroomName);
+
+        List<ExerciseSubmissionGeneralInfoDTO> exercisesInfo = new ArrayList<>();
+        //TODO: use mapper instead
+        if(exercises == null || exercises.isEmpty()){
+            exercises = null;
+        }else{
+            for(ExerciseSubmission exercise : exercises){
+                exercisesInfo.add(new ExerciseSubmissionGeneralInfoDTO
+                        (exercise.getId(), exercise.getSubject()));
+            }
+        }
+        //
+
+        return exercises;
     }
 }
