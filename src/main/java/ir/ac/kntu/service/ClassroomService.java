e7ae276364d74a48dad003ca757a66fa23b6c23c
SabaFathi <39127848+SabaFathi@users.noreply.github.com>
2019-12-13 02:28:12 +0330

    add new functions to find role of requester in the class and get detail info of class and get exercises of class

--- src/main/java/ir/ac/kntu/service/ClassroomService.java
+++ src/main/java/ir/ac/kntu/service/ClassroomService.java
@@ -2,10 +2,12 @@ package ir.ac.kntu.service;
 
 import ir.ac.kntu.exception.ClassroomNotFoundException;
 import ir.ac.kntu.model.Classroom;
+import ir.ac.kntu.model.ExerciseSubmission;
 import ir.ac.kntu.repository.ClassroomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -55,6 +57,66 @@ public class ClassroomService {
         } else {
             return findByLessonName(lessonName);
         }
+    }
+
+    public Classroom getClassroomDetailForRequester(final String classroomName){
+        Classroom classroom = findByClassroomName(classroomName);
+        String requesterUsername = UserService.getUsernameOfRequester();
+        if(isTeacherIn(requesterUsername, classroom) ||
+                isAssistantIn(requesterUsername, classroom)){
+            return classroom;
+        }else if(isStudentIn(requesterUsername, classroom)){
+            return classroom.withStudents(null);
+        }else{
+            return classroom.withStudents(null).withAssistant(null);
+        }
+    }
+
+    public List<ExerciseSubmission> getExercises(final String classroomName){
+        //TODO: refactor
+        Classroom classroom =findByClassroomName(classroomName);
+        String requesterUsername = UserService.getUsernameOfRequester();
+
+        List<ExerciseSubmission> allExercises = classroom.getExercises();
+        List<ExerciseSubmission> userExercise = new ArrayList<>();
+        boolean isOrganizer = isTeacherIn(requesterUsername, classroom) ||
+                isAssistantIn(requesterUsername, classroom);
+        for (ExerciseSubmission exercise : allExercises) {
+            switch (exercise.getAccessLevel()){
+                case ALL_STUDENTS:
+                    userExercise.add(exercise);
+                    break;
+                case ONLY_CREATOR:
+                    if(exercise.getCreator().getUsername().equals(requesterUsername)){
+                        userExercise.add(exercise);
+                    }
+                    break;
+
+                case CLASS_ORGANIZER:
+                    if(isOrganizer){
+                        userExercise.add(exercise);
+                    }
+                    break;
+            }
+        }
+
+        return userExercise;
+    }
+
+    private boolean isStudentIn(final String requesterUsername, final Classroom classroom){
+        return classroom.getStudents().stream()
+                .filter(student -> student.getUsername().equals(requesterUsername))
+                .count()>0;
+    }
+
+    private boolean isTeacherIn(final String requesterUsername, final Classroom classroom){
+        return classroom.getTeacher().getUsername().equals(requesterUsername);
+    }
+
+    private boolean isAssistantIn(final String requesterUsername, final Classroom classroom){
+        return classroom.getAssistant().stream()
+                .filter(student -> student.getUsername().equals(requesterUsername))
+                .count()>0;
     }
 
 }
