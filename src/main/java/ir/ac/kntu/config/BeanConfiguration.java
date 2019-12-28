934053281ba88d556164b1c2984c09f52aaae47b
Morteza Taghaddomi Masoumi <mortezataqaddomi99@gmail.com>
2019-12-28 17:50:38 +0330

    requesterRole changed to role

--- src/main/java/ir/ac/kntu/config/BeanConfiguration.java
+++ src/main/java/ir/ac/kntu/config/BeanConfiguration.java
@@ -19,7 +19,7 @@ public class BeanConfiguration {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("https://frontesh-static.herokuapp.com");
+                registry.addMapping("/**").allowedOrigins("http://localhost:8080");
                 // registry.addMapping("").allowedOrigins("http ://localhost:8080");
             }
         };
